10d30393a477e2256f7a9db511745b20360f1f89
Vadim Suleimanov <visulej2@mts.ru>
2017-04-10 17:26:01 +0300

    Styling Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import {HttpModule} from '@angular/http';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CardComponent } from './layout/card/card.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        DashboardComponent,
+        CardComponent
     ],
     imports: [
         BrowserModule,
