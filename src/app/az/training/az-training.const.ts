1b8056c29b6db079e981cc86aeac0e71397569de
Vadim Suleimanov <visulej2@mts.ru>
2017-05-12 11:48:09 +0300

    Create Train List

--- src/app/az/training/az-training.const.ts
+++ src/app/az/training/az-training.const.ts
@@ -43,12 +43,16 @@ class CAzTraining implements AzTraining {
 }
 
 export const AZ_TRAINING_LIST: AzTrainingList = [
-    new CAzTraining(1, [1, 2], 20, 2),
-    new CAzTraining(2, [3, 4], 20, 2),
-    new CAzTraining(3, [5, 6], 20, 2),
-    new CAzTraining(4, [7, 8], 20, 2),
-    new CAzTraining(5, [9, 10, 11], 20, 2),
-    new CAzTraining(6, [1, 2, 3, 4, 5], 40, 3),
-    new CAzTraining(7, [6, 7, 8, 9, 10, 11], 40, 3),
-    new CAzTraining(8, [], 60, 3),
+    new CAzTraining(1, [1, 2], 5, 2),
+    new CAzTraining(2, [3, 4], 5, 2),
+    new CAzTraining(3, [5, 6], 5, 2),
+    new CAzTraining(4, [7, 8], 5, 2),
+    new CAzTraining(5, [9, 10, 11], 5, 2),
+    new CAzTraining(6, [1, 2, 3, 4, 5], 4, 3),
+    new CAzTraining(7, [6, 7, 8, 9, 10, 11], 4, 3),
+    new CAzTraining(8, [], 3, 3),
 ];
+
+export const RANDOM_SORT = function(a: any, b: any) {
+    return Math.random() > 0.5 ? 1 : -1;
+}
--- src/app/az/training/az-training/az-training.component.ts
+++ src/app/az/training/az-training/az-training.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Subscription} from 'rxjs/Rx';
-import {AZ_TRAINING_LIST} from '../az-training.const';
+import {AZ_TRAINING_LIST, RANDOM_SORT} from '../az-training.const';
+import {az} from '../../az-list/az-list.const';
 
 
 @Component({
@@ -13,6 +14,10 @@ export class AzTrainingComponent implements OnInit, OnDestroy {
 
     azType: string;
     training: AzTraining;
+    step = 0;
+    totalCount = 0;
+    correctAnswer = 0;
+    trainList: Array<AzLiteral>;
     private routeSubs: Subscription;
 
     constructor(public route: ActivatedRoute, public router: Router) {
@@ -28,10 +33,46 @@ export class AzTrainingComponent implements OnInit, OnDestroy {
                 this.router.navigate(['/']);
             }
             this.training.clearProgress(this.azType);
+            this.trainList = [];
+
+            if (this.training.az.length) {
+                this.training.az.forEach(id => {
+                    az.find(f => {
+                        return f.id === id;
+                    }).literals.forEach(lit => {
+                        for (let i = 0; i < this.training.count; i++) {
+                            this.trainList.push(lit);
+                        }
+                    });
+                });
+            } else {
+                az.forEach(list => {
+                    list.literals.forEach(lit => {
+                        for (let i = 0; i < this.training.count; i++) {
+                            this.trainList.push(lit);
+                        }
+                    });
+                });
+            }
+
+            this.totalCount = this.trainList.length;
+            this.trainList.sort(RANDOM_SORT);
         });
     }
 
     ngOnDestroy() {
         this.routeSubs.unsubscribe();
+    }
+
+    newStep() {
+        this.step++;
+    }
+
+    get successProgress(): number {
+        return Math.round(this.correctAnswer / this.totalCount * 100);
+    }
+
+    get dangerProgress(): number {
+        return Math.round((this.step - this.correctAnswer) / this.totalCount * 100);
     }
 }
